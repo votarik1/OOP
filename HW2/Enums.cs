336ae37eca1ee5b38438de43eb716a3b370c570b
Alex <alexspb41@gmail.com>
2021-11-12 18:28:11 +0300

    ADD HW2

---
+++ HW2/Enums.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    enum AccountTypes { Standart, Premium, Super_Premium, Super_Mega_Premium, Super_Mega_Premium_Gold };
+}
