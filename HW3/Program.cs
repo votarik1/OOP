079839f5d642eb6eea7ba0a3b61e43b03e780df3
Alex <alexspb41@gmail.com>
2021-11-20 17:15:43 +0300

    ADD Task3

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.VisualBasic;
-using System;
-using System.Linq;
+﻿using System;
+using System.IO;
 using System.Text;
 
 namespace HW3
@@ -9,18 +8,49 @@ namespace HW3
     {
         static void Main(string[] args)
         {
-            BankAccount myBankAccount = new BankAccount(500000,AccountTypes.Super_Mega_Premium_Gold);
-            BankAccount notMyBankAccount = new BankAccount(500000);
-            myBankAccount.TopUp(notMyBankAccount, 25000);
-            myBankAccount.Print();
-            notMyBankAccount.Print();
-            Console.WriteLine(Reverse("Hello World"));
+            Console.WriteLine("Введите полный путь к файлу");
+            string pathRead = Console.ReadLine();
+            string fromFile = "";
+            try
+            {
+                fromFile = File.ReadAllText(pathRead);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Такого файла не существует");
+                return;
+            }
+            GetEmails(ref fromFile);
+            Console.WriteLine("Введите полный путь для файла результата");
+            string pathWrite = Console.ReadLine();
+            try
+            {
+                File.WriteAllText(pathWrite, fromFile);
+                Console.WriteLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Не удаётся записать файл");
+                return;
+            }
         }
 
+        public static void GetEmails(ref string s) //Разбивает весь текст на массив из слов. Ищет слова содержащие @. Компанует из них новую строку.
+        {
+            string[] words = s.Split(" ");
+            s = "";
+            foreach (var item in words)
+            {
+                if (item.Contains('@'))
+                {
+                    s += (item + Environment.NewLine);
+                }
+            }
+        }
         public static string Reverse(string input)
         {
             StringBuilder reverseString = new StringBuilder();
-            for (int i = input.Length-1; i >= 0 ; i--)
+            for (int i = input.Length - 1; i >= 0; i--)
             {
                 reverseString.Append(input[i]);
             }
