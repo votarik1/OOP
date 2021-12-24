05017139834b246c16c893e0707c902de68c71e5
Alex <alexspb41@gmail.com>
2021-12-24 23:24:07 +0300

    ADD HW7

---
+++ HW7/Coder/ACoder.cs
@@ -0,0 +1,67 @@
+﻿using Coder.Common;
+using System;
+using System.Text;
+
+namespace Coder
+{
+     public sealed class ACoder : ICoder
+    {
+        public string Encode(string str)
+        {
+            StringBuilder output = new StringBuilder();
+            foreach (char symbol in str)
+            {
+                if (symbol >= 'А' && symbol <= 'я') //проверка на буквы русского алфавита
+                {
+                    switch (symbol) //все значения увеличиваем на 1 кроме буквы Я. Ей присваеваем персонально.
+                    {
+                        case 'Я':
+                            output.Append('А');
+                            break;
+                        case 'я':
+                            output.Append('а');
+                            break;
+                        default:
+                            output.Append((char)(symbol+1)); 
+                            break;
+                    }
+                    continue;
+                }
+                output.Append(symbol);
+            }
+            return output.ToString();
+        }
+            
+            
+        
+
+        public string Decode(string str)    // Всё наоборот. Повторение кода - нехорошо, но как исправить - не знаю.
+        {
+            StringBuilder output = new StringBuilder();
+            foreach (char symbol in str)
+            {
+                if (symbol >= 'А' && symbol <= 'я')
+                {
+                    switch (symbol)
+                    {
+                        case 'А':
+                            output.Append('Я');
+                            break;
+                        case 'а':
+                            output.Append('я');
+                            break;
+                        default:
+                            output.Append((char)(symbol - 1));
+                            break;
+                    }
+                    continue;
+                }
+                output.Append(symbol);
+            }
+            return output.ToString();
+        }
+    }
+
+
+
+}
---
+++ HW7/Coder/Common/ICoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coder.Common
+{
+    public interface ICoder
+    {
+        string  Encode(string str);
+        string Decode(string str);
+    }
+}
